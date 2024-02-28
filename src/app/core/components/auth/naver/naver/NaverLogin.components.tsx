76592423896703cea62edf3fdc41f72e415fbb95
Lee-ji-soo <nbsp3@naver.com>
2024-02-28 10:26:05 +0900

    feat: change redirect url host

--- src/app/core/components/auth/naver/naver/NaverLogin.components.tsx
+++ src/app/core/components/auth/naver/naver/NaverLogin.components.tsx
@@ -1,6 +1,6 @@
 const _signInButton = () => {
   return (
-    <a href='http://13.210.209.64:8080/oauth2/authorization/naver?redirect_uri=http://localhost:3000/auth'>
+    <a href="http://13.210.209.64:8080/oauth2/authorization/naver?redirect_uri=http://127.0.0.1:3000/auth">
       네이버
     </a>
   );
